ede45adc2a4b104659f40ac26e52b1b838089a64
Bartłomiej Buchała <bbuchala93@gmail.com>
2017-04-15 21:47:16 +0200

    Object Generator template project

---
+++ BinPackingProblem/GUI.Helpers/BaseNotifyPropertyChanged.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Helpers
+{
+    public class BaseNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            // Take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;              
+
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
