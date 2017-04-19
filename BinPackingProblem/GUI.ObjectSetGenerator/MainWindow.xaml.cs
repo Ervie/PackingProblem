3fb180e7a1091368c2bd3e4448b9cf86dff16e67
Bartłomiej Buchała <bbuchala93@gmail.com>
2017-04-19 22:32:39 +0200

    Small changes for GUI project

--- BinPackingProblem/GUI.ObjectSetGenerator/MainWindow.xaml.cs
+++ BinPackingProblem/GUI.ObjectSetGenerator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI.ObjectSetGenerator.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace GUI.ObjectSetGenerator
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainViewModel();
         }
     }
 }
---
+++ BinPackingProblem/GUI.ObjectSetGenerator/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GUI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ObjectSetGenerator.ViewModel
+{
+    class MainViewModel: BaseNotifyPropertyChanged
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
