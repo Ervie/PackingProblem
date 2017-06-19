a9bf1037fdb6d050809c5521e888ba7a55b5090b
Bartłomiej Buchała <bbuchala93@gmail.com>
2017-06-19 22:38:41 +0200

    Some code for console version.

--- BinPackingProblem/Console.BinPackingProblem/Program.cs
+++ BinPackingProblem/Console.BinPackingProblem/Program.cs
@@ -1,13 +1,35 @@
-﻿using System;
+﻿using Logic.Algorithms;
+using Logic.Algorithms.Enums;
+using Logic.Algorithms.Structs;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Console.BinPackingProblem
 {
-	class Program
+
+
+	internal class Program
 	{
+		public static string[] arguments = { "-d", "-t", "-o", "-s" };
+		public static string[] dimensionalities = { "3", "2",};
+		public static string[] algorithTypes = { "ShN", "ShF", "ShBA", "ShBW", "ShBH", "ShWA", "ShWW", "SkBL", "SkBFFC", "SkBFBC",
+		"GBA", "GWA", "GBSS", "GWSS", "GBLS", "GWLS", "GBL"};
+		public static string[] orderings = { "N", "W", "H", "D", "L", "S", "P", "A", "V", "PA", "SA", "SR", "WHR", "WHD",
+		"WDR", "WDD", "HDR", "HDD"};
+		public static string[] splittingStrategies = { "MinA", "MaxA", "LA", "LL", "SA", "SL" };
+
+		public static string InputFilePath { get; set; }
+
+		public static string OutputFilePath { get; set; }
+
+		public static AlgorithmProperties Properties { get; set; }
+
+		public static ObjectOrdering Ordering { get; set; }
+
 		static void Main(string[] args)
 		{
 			if (args.Count() < 2)
@@ -17,17 +39,222 @@ namespace Console.BinPackingProblem
 			}
 			else
 			{
-				RecognizeArguments(args);
+				try
+				{
+					ProcessArguments(args);
+				}
+				catch (Exception er)
+				{
+					System.Console.WriteLine($"Error during computing: {er.Message}" );
+					DisplayHelp();
+				}
 			}
 
 			System.Console.ReadKey();
 		}
 
-		private static void RecognizeArguments(string[] args)
+		private static void ProcessArguments(string[] args)
 		{
-			throw new NotImplementedException();
+			InputFilePath = args[0];
+
+			string pathWithoutFile = Path.GetDirectoryName(InputFilePath);
+
+			if (!Directory.Exists(pathWithoutFile))
+			{
+				Directory.CreateDirectory(pathWithoutFile);
+			}
+
+			if (!File.Exists(InputFilePath))
+			{
+				File.Create(InputFilePath);
+			}
+
+			OutputFilePath = Path.GetDirectoryName(InputFilePath) + args.Last();
+
+			if (!OutputFilePath.EndsWith(".csv"))
+				OutputFilePath += ".csv";
+
+			if (args.Count() > 3 && arguments.Contains(args[2]))
+				RecognizePair(args[2], args[3]);
+			if (args.Count() > 5 && arguments.Contains(args[4]))
+				RecognizePair(args[4], args[5]);
+			if (args.Count() > 7 && arguments.Contains(args[6]))
+				RecognizePair(args[6], args[7]);
+			if (args.Count() > 9 && arguments.Contains(args[8]))
+				RecognizePair(args[8], args[9]);
+
+
+			var factory = new AlgorithmFactory();
+			//var algorithm = factory.Create(Properties);
 		}
 
+		private static void RecognizePair(string argumentType, string argumentValue)
+		{
+			switch (argumentType)
+			{
+				case ("-d"):
+					Properties.Dimensionality = SetDimensionality(argumentValue);
+					break;
+				case ("-t"):
+					Properties.Family = SetFamily(argumentValue);
+					Properties.AlgorithmType = SetObjectFittingStrategy(argumentValue);
+					break;
+				case ("-o"):
+					Ordering = SetOrdering(argumentValue);
+					break;
+				case ("-s"):
+					Properties.SplittingStrategy = SetSplittingStrategy(argumentValue);
+					break;
+			}
+		}
+
+		private static AlgorithmDimensionality SetDimensionality(string argumentValue)
+		{
+			switch(argumentValue)
+			{
+				case ("2"):
+					return AlgorithmDimensionality.TwoDimensional;
+				case ("3"):
+					return AlgorithmDimensionality.ThreeDimensional;
+				default:
+					throw new InvalidArgumentException();
+			}
+		}
+
+		private static AlgorithmFamily SetFamily(string argumentValue)
+		{
+			switch (argumentValue)
+			{
+				case ("ShN"):
+				case ("ShF"):
+				case ("ShBA"):
+				case ("ShBH"):
+				case ("ShBW"):
+				case ("ShWA"):
+				case ("ShWW"):
+					return AlgorithmFamily.Shelf;
+				case ("SkBL"):
+				case ("SkBFFC"):
+				case ("SkBFBC"):
+					return AlgorithmFamily.Skyline;
+				case ("GBA"):
+				case ("GWA"):
+				case ("GBSS"):
+				case ("GWSS"):
+				case ("GWLS"):
+				case ("GBLS"):
+				case ("GWBL"):
+					return AlgorithmFamily.GuillotineCut;
+				default:
+					throw new InvalidArgumentException();
+			}
+		}
+
+		private static ObjectFittingStrategy SetObjectFittingStrategy(string argumentValue)
+		{
+			switch (argumentValue)
+			{
+				case ("ShN"):
+					return ObjectFittingStrategy.NextFit;
+				case ("ShF"):
+					return ObjectFittingStrategy.FirstFit;
+				case ("ShBA"):
+				case ("GBA"):
+					return ObjectFittingStrategy.BestAreaFit;
+				case ("ShBH"):
+					return ObjectFittingStrategy.BestHeightFit;
+				case ("ShBW"):
+					return ObjectFittingStrategy.BestWidthFit;
+				case ("ShWA"):
+				case ("GWA"):
+					return ObjectFittingStrategy.WorstAreaFit;
+				case ("ShWW"):
+					return ObjectFittingStrategy.WorstWidthFit;
+				case ("SkBL"):
+				case ("GWBL"):
+					return ObjectFittingStrategy.BottomLeft;
+				case ("SkBFFC"):
+					return ObjectFittingStrategy.BestFitFirstContainer;
+				case ("SkBFBC"):
+					return ObjectFittingStrategy.BestFitBestContainer;
+				case ("GBSS"):
+					return ObjectFittingStrategy.BestShortSideFit;
+				case ("GWSS"):
+					return ObjectFittingStrategy.WorstShortSideFit;
+				case ("GWLS"):
+					return ObjectFittingStrategy.WorstLongSideFit;
+				case ("GBLS"):
+					return ObjectFittingStrategy.BestLongSideFit;
+				default:
+					throw new InvalidArgumentException();
+			}
+		}
+
+		private static ContainerSplittingStrategy SetSplittingStrategy(string argumentValue)
+		{
+			switch (argumentValue)
+			{
+				case ("MaxA"):
+					return ContainerSplittingStrategy.MaxAreaSplitRule;
+				case ("MinA"):
+					return ContainerSplittingStrategy.MinAreaSplitRule;
+				case ("LA"):
+					return ContainerSplittingStrategy.LongerAxisSplitRule;
+				case ("LL"):
+					return ContainerSplittingStrategy.LongerLeftoverAxisSplitRule;
+				case ("SA"):
+					return ContainerSplittingStrategy.ShorterAxisSplitRule;
+				case ("SL"):
+					return ContainerSplittingStrategy.ShorterLeftoverAxisSplitRule;
+				default:
+					throw new InvalidArgumentException();
+			}
+		}
+
+		private static ObjectOrdering SetOrdering(string argumentValue)
+		{
+			switch (argumentValue)
+			{
+				case ("N"):
+					return ObjectOrdering.None;
+				case ("W"):
+					return ObjectOrdering.ByWidth;
+				case ("H"):
+					return ObjectOrdering.ByHeight;
+				case ("D"):
+					return ObjectOrdering.ByDepth;
+				case ("L"):
+					return ObjectOrdering.ByLongest;
+				case ("S"):
+					return ObjectOrdering.ByShortest;
+				case ("P"):
+					return ObjectOrdering.ByPerimeter;
+				case ("A"):
+					return ObjectOrdering.ByArea;
+				case ("V"):
+					return ObjectOrdering.ByVolume;
+				case ("SA"):
+					return ObjectOrdering.BySurfaceArea;
+				case ("SR"):
+					return ObjectOrdering.BySideRatio;
+				case ("SD"):
+					return ObjectOrdering.BySideDifference;
+				case ("WHR"):
+					return ObjectOrdering.ByWidthHeightRatio;
+				case ("WHD"):
+					return ObjectOrdering.ByWidthHeightDifference;
+				case ("WDR"):
+					return ObjectOrdering.ByWidthDepthRatio;
+				case ("WDD"):
+					return ObjectOrdering.ByWidthDepthDifference;
+				case ("HDR"):
+					return ObjectOrdering.ByHeightDepthRatio;
+				case ("HDD"):
+					return ObjectOrdering.ByHeightDepthDifference;
+				default:
+					throw new InvalidArgumentException();
+			}
+		}
 		private static void DisplayHelp()
 		{
 			StringBuilder sb = new StringBuilder();
@@ -56,7 +283,7 @@ namespace Console.BinPackingProblem
 			sb.AppendLine("GWSS - Guillotine cut worst short side");
 			sb.AppendLine("GBLS - Guillotine cut best long side");
 			sb.AppendLine("GWLS - Guillotine cut worst long side");
-			sb.AppendLine("GWLS - Guillotine cut Bottom left");
+			sb.AppendLine("GBL - Guillotine cut Bottom left");
 			sb.AppendLine("");
 			sb.AppendLine("Ordering values:");
 			sb.AppendLine("N - none");
@@ -67,6 +294,7 @@ namespace Console.BinPackingProblem
 			sb.AppendLine("S - By shortest");
 			sb.AppendLine("P - By perimeter");
 			sb.AppendLine("A - By area");
+			sb.AppendLine("V - By volume");
 			sb.AppendLine("SA - By surface area");
 			sb.AppendLine("SR - By side ratio");
 			sb.AppendLine("SD - By side difference");
