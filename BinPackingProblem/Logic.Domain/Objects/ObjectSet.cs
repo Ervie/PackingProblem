36bf06a232cf2c18652f6050afcc75f5fc6b154c
Bartłomiej Buchała <bbuchala93@gmail.com>
2017-07-16 19:13:52 +0200

    Fixing maximum number of objects.

--- BinPackingProblem/Logic.Domain/Objects/ObjectSet.cs
+++ BinPackingProblem/Logic.Domain/Objects/ObjectSet.cs
@@ -9,5 +9,42 @@ namespace Logic.Domain.Objects
     [Serializable]
     public class ObjectSet: List<Object>
     {
-    }
+		public int GetHeighestHeight()
+		{
+			if (this.First() is Object2D)
+			{
+				return this.Max(x => (x as Object2D).Height);
+			}
+			else if (this.First() is Object3D)
+			{
+				return this.Max(x => (x as Object3D).Height);
+			}
+			else
+				return -1;
+		}
+
+		public int GetWidestWidth()
+		{
+			if (this.First() is Object2D)
+			{
+				return this.Max(x => (x as Object2D).Width);
+			}
+			else if (this.First() is Object3D)
+			{
+				return this.Max(x => (x as Object3D).Width);
+			}
+			else
+				return -1;
+		}
+
+		public int GetDeepestDepth()
+		{
+			if (this.First() is Object3D)
+			{
+				return this.Max(x => (x as Object3D).Width);
+			}
+			else
+				return -1;
+		}
+	}
 }
